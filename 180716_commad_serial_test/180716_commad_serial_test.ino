8c215a8839f93d8d348a82d3cc4ab3249e73a1a7
takurx <taku3wzr@gmail.com>
2018-07-17 21:57:18 +0900

    test of changing  baurate
    change baurate 9600 -> 38400 bps test

--- 180716_commad_serial_test/180716_commad_serial_test.ino
+++ 180716_commad_serial_test/180716_commad_serial_test.ino
@@ -1,25 +1,50 @@
 // http://d.hatena.ne.jp/kougaku-navi/20141105/p1
 
 void setup() {
+  String str;
   Serial.begin(38400);
-  Serial.setTimeout(15000UL); // タイムアウト設定（15秒）
+  
+  Serial2.begin(9600);
+  //Serial2.begin(38400);
+  Serial2.print("BCD0\r\n");
+  str = Serial2.readStringUntil('\r');
+  Serial.println(str);
+  //Serial2.print("BST5000001\r\n"); //0001:9600bps
+  Serial2.print("BST5000003\r\n"); //0003:38400bps
+  str = Serial2.readStringUntil('\r');
+  Serial.println(str);
+  Serial2.print("BCD3\r\n");
+  str = Serial2.readStringUntil('\r');
+  Serial.println(str);
+  
+  //Serial2.begin(9600);
+  Serial2.begin(38400);
+  //Serial2.setTimeout(15000UL); // タイムアウト設定（15秒）
   pinMode(10,OUTPUT);
 }
 
 void loop() {
-  if ( Serial.available() > 0 ) {
-    String str = Serial.readStringUntil('\r');
+  if ( Serial2.available() > 0 ) {
+    String str = Serial2.readStringUntil('\r');
 
-    Serial.println();
+    Serial2.println();
     if ( str == "on") {
       digitalWrite(10,HIGH);
+      Serial2.println("> ok : LED13 turn on");
+
       Serial.println("> ok : LED13 turn on");
     }
     else if ( str=="off" ) {
       digitalWrite(10,LOW);
+      Serial2.println("> ok : LED13 turn off");
+
       Serial.println("> ok : LED13 turn off");
     }
     else {
+      Serial2.print("> error [");
+      Serial2.print(str);
+      Serial2.println("]");
+
       Serial.print("> error [");
       Serial.print(str);
       Serial.println("]");
