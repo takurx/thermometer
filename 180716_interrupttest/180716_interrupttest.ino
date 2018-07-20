8cfe330ce43808572c8e040398f56ae89be2333d
takuwz <taku3wzr@gmail.com>
2018-07-20 21:26:42 +0900

    replace mstimer2 to duetimer
    replace mstimer2 to duetimer

--- 180716_interrupttest/180716_interrupttest.ino
+++ 180716_interrupttest/180716_interrupttest.ino
@@ -1,10 +1,12 @@
+#include <DueTimer.h>
+
 // https://miso-engine.hatenablog.com/entry/2015/07/20/221014
-#include <MsTimer2.h>
+// #include <MsTimer2.h>
 
 volatile int state = LOW;
 
 void timerFire() {
-  //1000ms毎にここが呼び出される
+  //500ms毎にここが呼び出される
   state = !state;
   digitalWrite(LED_BUILTIN, state);   // turn the LED on (HIGH is the voltage level)
 }
@@ -13,9 +15,9 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   
-  //100ms毎にtimer発火
-  MsTimer2::set(1000, timerFire);
-  MsTimer2::start();
+  //500ms毎にtimer発火
+  Timer2.attachInterrupt(timerFire);
+  Timer2.start(100000); // 500ms = 500,000us
 }
 
 void loop() {
